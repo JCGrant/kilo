76c7648de3408c4d8fe679dc36df5609397cda4e
JCGrant <jamescolin.grant@gmail.com>
2017-07-20 15:40:49 +0100

    Make status message disappear when user presses a key after 5 seconds

--- kilo.c
+++ kilo.c
@@ -363,7 +363,7 @@ void editorDrawMessageBar(struct abuf *ab) {
   abAppend(ab, "\x1b[K", 3);
   int msglen = strlen(E.statusmsg);
   if (msglen > E.screencols) msglen = E.screencols;
-  if (msglen) {
+  if (msglen && time(NULL) - E.statusmsg_time < 5) {
     abAppend(ab, E.statusmsg, msglen);
   }
 }
