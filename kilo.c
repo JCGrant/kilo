e1149a4180da80413339952f0db45641b73b4a7a
JCGrant <jamescolin.grant@gmail.com>
2017-07-24 14:35:26 +0100

    Reinitialise E.dirty to 0 on editor open save

--- kilo.c
+++ kilo.c
@@ -300,6 +300,7 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave() {
@@ -312,6 +313,7 @@ void editorSave() {
       if (write(fd, buf, len) == len) {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
